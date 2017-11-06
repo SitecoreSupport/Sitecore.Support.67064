cf1a0b9b46e1eab9e294a16f2cf2702e83053852
Dmitry Kolinchuk <dmko@sitecore.net>
2017-11-06 11:14:32 +0200

    Original code

---
+++ src/Sitecore.Support.67064/DatePickerField.cs
@@ -0,0 +1,63 @@
+﻿using Sitecore.Forms.Mvc.TypeConverters;
+using Sitecore.WFFM.Abstractions.Actions;
+using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Sitecore.Forms.Mvc.ViewModels.Fields
+{
+  public class DatePickerField : ValuedFieldViewModel<string>
+  {
+    public override string Value
+    {
+      get
+      {
+        return base.Value;
+      }
+      set
+      {
+        base.Value = (DateUtil.IsIsoDate(value) ? DateUtil.IsoDateToDateTime(value).ToString(this.DateFormat ?? "yy-MM-dd") : value);
+      }
+    }
+
+    public string DateFormat
+    {
+      get;
+      set;
+    }
+
+    [TypeConverter(typeof(IsoDateTimeConverter))]
+    public DateTime StartDate
+    {
+      get;
+      set;
+    }
+
+    [TypeConverter(typeof(IsoDateTimeConverter))]
+    public DateTime EndDate
+    {
+      get;
+      set;
+    }
+
+    public DatePickerField()
+    {
+      this.DateFormat = "yy-MM-dd";
+      this.StartDate = DateUtil.IsoDateToDateTime("20000101T120000");
+      this.EndDate = DateTime.Now.AddYears(1).Date;
+    }
+
+    public override void Initialize()
+    {
+      if (string.IsNullOrEmpty(this.Value))
+      {
+        this.Value = DateTime.Now.ToString(this.DateFormat);
+      }
+    }
+
+    public override ControlResult GetResult()
+    {
+      return new ControlResult(base.FieldItemId, this.Title, DateUtil.ToIsoDate(DateTime.ParseExact(this.Value, this.DateFormat, DateTimeFormatInfo.InvariantInfo)), this.ResultParameters, false);
+    }
+  }
+}
